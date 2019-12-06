5b1792f1385d59ecadc1853017556f74b1679bdf
ryanbevan3 <35648052+ryanbevan3@users.noreply.github.com>
2019-12-05 18:55:22 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function goHome(){
+    window.location.href = "index.html";
+}
+
+function goPictures(){
+    window.location.href = "pictures.html";
+}
+
+function goGameplay(){
+    window.location.href = "gameplay.html";
+}
+
+function goReviews(){
+    window.location.href = "reviews.html";
+}
+
+function goYoutube(){
+    window.location.href = "youtube.html";
+}
